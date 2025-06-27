eb685170d576dc1b2893ea7ce7c05324f2e3a938
Abhishek2010dev <abhishekkumar2010dev@gmail.com>
2025-06-27 21:51:30 +0530

    basic middleware function

--- context.go
+++ context.go
@@ -3,5 +3,32 @@ package zeno
 import "github.com/valyala/fasthttp"
 
 type Context struct {
-	*fasthttp.RequestCtx
+	RequestCtx *fasthttp.RequestCtx
+
+	zeno     *Zeno
+	pnames   []string
+	pvalues  []string
+	index    int
+	handlers []Handler
+}
+
+func (c *Context) Next() error {
+	c.index++
+	for n := len(c.handlers); c.index < n; c.index++ {
+		if err := c.handlers[c.index](c); err != nil {
+			return err
+		}
+	}
+	return nil
+}
+
+func (c *Context) Abort() {
+	c.index = len(c.handlers)
+}
+
+func (c *Context) URL(route string, pairs ...interface{}) string {
+	if r := c.zeno.routes[route]; r != nil {
+		return r.URL(pairs...)
+	}
+	return ""
 }
